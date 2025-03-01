35a3d5dbca026a75f4e421b685799322055acd97
Chethan Karandikar <90787087+chethankarandikar@users.noreply.github.com>
2025-02-28 23:54:40 -0600

    Fixed padding and navbar

--- hackillinois-front/src/components/Navbar.jsx
+++ hackillinois-front/src/components/Navbar.jsx
@@ -9,21 +9,7 @@ const Navbar = () => {
       
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal space-x-8 px-1">
-          <li>
-            <Link to="/scholarships" className="text-gray-300 hover:text-purple-400 px-2 py-1">
-              Scholarships
-            </Link>
-          </li>
-          <li>
-            <Link to="/how-it-works" className="text-gray-300 hover:text-purple-400 px-2 py-1">
-              How It Works
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="text-gray-300 hover:text-purple-400 px-2 py-1">
-              About
-            </Link>
-          </li>
+          
         </ul>
       </div>
       
